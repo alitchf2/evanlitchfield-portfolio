33fcd6e02c9a8f2e8f362214dfc49bc51bae4902
alitchf2 <alitchf2@charlotte.edu>
2026-09-01 13:35:16 -0400

    testing git actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center p-6">
-      <h1 className="text-4xl font-bold mb-4">Welcome to evanlitchfield.com</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to evanlitchfield.com!</h1>
       <p>The offical website of Evan Litchfield, future Broadway star with the greatest eldest brother ever!</p>
     </div>
   );
